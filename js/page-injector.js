2f103525cac55a202ef9b6677f0c8632b95c2f74
Christopher Hunt <chrahunt@gmail.com>
2014-10-27 19:08:16 -0400

    Add ability to inject scripts into page.

---
+++ js/page-injector.js
@@ -0,0 +1,28 @@
+// Remove self.
+function removeScript() {
+  this.parentNode.removeChild(this);
+}
+
+// Inject script given by path into page.
+function injectScript(path) {
+  var script = document.createElement('script');
+  script.setAttribute("type", "application/javascript");
+  script.src = path;
+  script.onload = removeScript;
+  (document.head||document.documentElement).appendChild(script);
+}
+
+// If we're in a game, as evidenced by there being a port number, inject the scripts.
+if(document.URL.search(/\.\w+:/) >= 0) {
+  // Get scripts from index link.
+  chrome.storage.local.get('index', function(items) {
+    if (!chrome.runtime.lastError) {
+      var listLink = items.index;
+      // No changes needed for json, parsed automatically
+      $.get(listLink, function(scripts) {
+        console.log("Injecting " + scripts.length + " scripts.");
+        scripts.forEach(injectScript);
+      }, "json");
+    }
+  });
+}
